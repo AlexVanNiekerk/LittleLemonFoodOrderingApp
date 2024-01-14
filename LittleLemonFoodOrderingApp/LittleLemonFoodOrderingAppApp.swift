62df9fdfeb9995a359fe2b7f23fa9d3ea4c37057
AlexVanNiekerk <alexvanniekerk57@gmail.com>
2024-01-14 14:48:42 +0200

    Initial Commit

---
+++ LittleLemonFoodOrderingApp/LittleLemonFoodOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonFoodOrderingAppApp.swift
+//  LittleLemonFoodOrderingApp
+//
+//  Created by Alex van Niekerk on 2024/01/14.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonFoodOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
